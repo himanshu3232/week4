bf240c6ac5e9d2ae6fb83fa0fb6fef186b7f5a82
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-14 11:42:07 +0530

    [Himanshu Ranjan] Add. nested try catch

---
+++ day4/src/main/java/org/capgemini/nested_try_catch/NestedTryCatchExample.java
@@ -0,0 +1,31 @@
+package org.capgemini.nested_try_catch;
+
+import java.util.Scanner;
+
+public class NestedTryCatchExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = {10, 20, 30, 40, 50};
+
+        try {
+            System.out.print("Enter index: ");
+            int index = scanner.nextInt();
+
+            try {
+                System.out.print("Enter divisor: ");
+                int divisor = scanner.nextInt();
+
+                int result = array[index] / divisor;
+                System.out.println("Result: " + result);
+            } catch (ArithmeticException e) {
+                System.out.println("Cannot divide by zero!");
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid array index!");
+        } finally {
+            scanner.close();
+            System.out.println("Operation completed");
+        }
+    }
+}
+
