0b48b9536f59b333c1f5d2e0c3aa13b835604f4b
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-17 03:29:48 +0530

    [Himanshu Ranjan] Calculator test

---
+++ day5/src/test/java/CalculatorTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.example.junit.basic.Calculator;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    Calculator calculator = new Calculator();
+
+    @Test
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(-1, calculator.add(-2, 1));
+    }
+
+    @Test
+    void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+        assertEquals(-3, calculator.subtract(-2, 1));
+    }
+
+    @Test
+    void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+        assertEquals(-2, calculator.multiply(-2, 1));
+    }
+
+    @Test
+    void testDivide() {
+        assertEquals(2, calculator.divide(6, 3));
+        assertEquals(-2, calculator.divide(-6, 3));
+    }
+
+    @Test
+    void testDivideByZero() {
+        Exception exception = assertThrows(ArithmeticException.class, () -> calculator.divide(1, 0));
+        assertEquals("Division by zero is not allowed", exception.getMessage());
+    }
+}
