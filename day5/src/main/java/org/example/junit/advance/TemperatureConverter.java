2370c2d1e83f44ccb385d41391267d8bd9a9d2bc
himanshu3232 <himanshu2000ranjan@gmail.com>
2025-02-17 04:01:41 +0530

    [Himanshu Ranjan] temperature converter tests

---
+++ day5/src/main/java/org/example/junit/advance/TemperatureConverter.java
@@ -0,0 +1,11 @@
+package org.example.junit.advance;
+
+public class TemperatureConverter {
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
